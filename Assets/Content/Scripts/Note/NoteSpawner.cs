3519a368d6963829863e4783ec01fade0c538979
KatoShiina <katoshiina812@gmail.com>
2025-01-09 22:13:15 +0100

    Added all notes, made simple track

--- Assets/Content/Scripts/Note/NoteSpawner.cs
+++ Assets/Content/Scripts/Note/NoteSpawner.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class NoteSpawner : MonoBehaviour
 {
-    public GameObject scratchL, scratchR, holdL, holdR, bossChange, avoid;
+    public GameObject scratchL, scratchR, bossChange, avoid;
     private Track[] tracks;
     List<GameObject> activeNotes = new List<GameObject>();
+    public LayerMask whatIsNote;
 
     public int[] notesPerBeat;
     public int[] trackNum;
 
     public int[] noteTypes;
 
-    public float noteVel;
+    public float noteOffset;
 
+    private RaycastHit2D hit;
+    private Vector3 mousePosRaw;
+    private Vector2 mousePos;
+
+    GameObject tmpNote = null;
+
+
+    private bool hasPickedUpNote = false;
     private void Start()
     {
         tracks = GameObject.FindObjectsOfType<Track>();
@@ -23,20 +32,23 @@ public class NoteSpawner : MonoBehaviour
 
     private void Update()
     {
-        MoveAllNotes();
-    }
+        mousePosRaw = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector3(mousePosRaw.x, mousePosRaw.y, 0f);    
 
-    void SpawnMusicTrack(float beatsNum, int[] notesPerBeat, int[] trackNum, int[] noteTypes)
-    {
-        for(int i = 0;  i < beatsNum; i++)
+        if (Input.GetMouseButton(0) && Physics2D.Raycast(mousePosRaw, Vector3.zero, Mathf.Infinity, whatIsNote))
         {
-            if (notesPerBeat.Length < i)
-            {
-                for(int j = 0; j <  notesPerBeat.Length; j++)
-                {
-                    activeNotes.Add(Instantiate(scratchL, tracks[trackNum[i]].startPos, Quaternion.identity));
-                }
-            }
+            hit = Physics2D.Raycast(mousePosRaw, Vector3.zero, Mathf.Infinity, whatIsNote);
+            DragNote();
+        }
+        if(Input.GetMouseButtonUp(0) && Physics2D.Raycast(mousePosRaw, Vector3.zero, Mathf.Infinity, whatIsNote) && hasPickedUpNote)
+        {
+            hasPickedUpNote = false;
+            CheckClosestTrack(tmpNote);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveAllNotes();
         }
     }
 
@@ -44,7 +56,52 @@ public class NoteSpawner : MonoBehaviour
     {
         for(int i = 0;  i < activeNotes.Count; i++) 
         {
-            activeNotes[i].transform.position += new Vector3 (noteVel * Time.deltaTime, 0, 0);
+            activeNotes[i].transform.position += new Vector3 (noteOffset, 0, 0);
         }
+        for(int i = 0; i < tracks.Length; i++)
+        {
+            tracks[i].isOccupied = false;
+        }
+    }
+
+    //gotta work on this one(fking notes don't follow me if i move too fast lol)
+    void DragNote()
+    {
+        if (!hasPickedUpNote)
+        {
+            tmpNote = Instantiate(hit.transform.gameObject, mousePos, Quaternion.identity);
+            hasPickedUpNote = true;
+            activeNotes.Add(tmpNote);
+        }
+        if (hasPickedUpNote)
+        {
+            tmpNote.transform.position = new Vector3(mousePos.x, mousePos.y, 0f);
+        }    
+    }
+
+    void CheckClosestTrack(GameObject note)
+    {
+        float closestDistance = Mathf.Infinity;
+        int closestTrackID = -1;
+
+        for(int i = 0; i < tracks.Length; i++)
+        {
+            if (Vector3.Distance(tracks[i].transform.position, note.transform.position) < closestDistance)
+            {
+                closestDistance = Vector3.Distance(tracks[i].transform.position, note.transform.position);
+                closestTrackID = i;
+            }
+        }
+        if (!tracks[closestTrackID].isOccupied)
+        {
+            note.transform.position = tracks[closestTrackID].startPos;
+            tracks[closestTrackID].isOccupied = true;
+        }
+        else
+        {
+            Destroy(note);
+            Debug.Log("Track Already Occupied");
+        }
+        
     }
 }
