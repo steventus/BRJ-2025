b3e04705ca92cfd159568ea1c980d7fae9c31cc8
YellowFeatherStudio <109194104+YellowFeatherStudio@users.noreply.github.com>
2025-01-15 15:39:01 -0800

    added perfect hits

--- Assets/Content/Scripts/Metronome.cs
+++ Assets/Content/Scripts/Metronome.cs
@@ -12,10 +12,12 @@ public class Metronome : MonoBehaviour
     public Transform nextBeat;
     public float lerpSpeed;
 
+    public GameObject perfectMessage;
     public GameObject hitMessage;
     public GameObject missMessage;
 
     public float minThresholdForNoteHit = 0.4f;
+    public float perfectHitThreshold = 0.2f;
     void Start() {
         beatIndex = 0;
     }
@@ -33,30 +35,42 @@ public class Metronome : MonoBehaviour
         }
 
         metronomeLine.position = Vector3.Lerp(metronomeLine.position, nextBeat.position, Time.deltaTime * lerpSpeed);
-
     }
 
     public void CheckIfInputIsOnBeat() {
+        int perfectHitDmg = 3;
+        int hitDmg = 1;
+
         float inputPressDistanceFromBeat = Mathf.Abs((float)beatIndex - conductor.loopPositionInBeats); 
-        if(inputPressDistanceFromBeat < minThresholdForNoteHit) {
+        Debug.Log(inputPressDistanceFromBeat);
+        if(inputPressDistanceFromBeat < perfectHitThreshold) {
+            perfectMessage.SetActive(true);
+            hitMessage.SetActive(false);
+            missMessage.SetActive(false);
+    
+            Events.OnSuccessfulNoteHit?.Invoke(perfectHitDmg);
+        }
+        else if(inputPressDistanceFromBeat < minThresholdForNoteHit) {
             hitMessage.SetActive(true);
+            perfectMessage.SetActive(false);
             missMessage.SetActive(false);
 
             //invoke successful input event
-            Events.OnSuccessfulNoteHit?.Invoke(2);
+            Events.OnSuccessfulNoteHit?.Invoke(hitDmg);
         }
         else {
             hitMessage.SetActive(false);
             missMessage.SetActive(true);
          
             //invoke failed input event
-            Events.OnUnsuccessfulNoteHit?.Invoke(2);
+            Events.OnUnsuccessfulNoteHit?.Invoke(hitDmg);
         }
 
         Invoke("DisableMessages", 0.25f);
     }
 
     void DisableMessages() {
+        perfectMessage.SetActive(false);
         hitMessage.SetActive(false);
         missMessage.SetActive(false);
     }
