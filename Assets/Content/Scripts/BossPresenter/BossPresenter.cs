0c289bfeb325e006e3097e9be789232514df5db2
Steventus <56802489+steventus@users.noreply.github.com>
2025-01-16 18:59:12 +0800

    Dictionary and build an interface

--- Assets/Content/Scripts/BossPresenter/BossPresenter.cs
+++ Assets/Content/Scripts/BossPresenter/BossPresenter.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
+//TODO: TO BE INTEGRATED WITH KATO'S NOTE TYPES
+public class NoteType
+{
+    public enum Note
+    {
+        scratch,
+        hold,
+        bad,
+        change
+    }
+}
 public class BossPresenter : MonoBehaviour
 {
     public Sprite scratchNoteSprite;
@@ -16,6 +29,13 @@ public class BossPresenter : MonoBehaviour
         badNote,
         changeNote,
     }
+    Dictionary<NoteType.Note, DancePose> danceDictionary = new Dictionary<NoteType.Note, DancePose>(){
+        {NoteType.Note.scratch, DancePose.scratchNote},
+        {NoteType.Note.hold, DancePose.holdNote},
+        {NoteType.Note.bad, DancePose.badNote},
+        {NoteType.Note.change, DancePose.changeNote}
+    };
+
     [SerializeField] private SpriteRenderer spriteRenderer;
 
     void Start()
@@ -25,15 +45,47 @@ public class BossPresenter : MonoBehaviour
 
     void Update()
     {
+        #region TO BE REMOVED IN PRODUCTION
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            CheckNoteType(NoteType.Note.scratch, true);
+        }
 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CheckNoteType(NoteType.Note.hold);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            CheckNoteType(NoteType.Note.bad);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CheckNoteType(NoteType.Note.change);
+        }
+        #endregion
     }
-    //TODO: Pull from judgement line/notes hit with observer design pattern
+
+    //When chart is created with notes being filled in, run this method for each note filling in.
+    public void CheckNoteType(NoteType.Note noteType, bool ifRight = false)
+    {
+        //Todo: Need to be connected with Kato's chart loader - so the dance will cue appropriately
+        DancePose _chosenDance;
+        if (danceDictionary.TryGetValue(noteType, out _chosenDance))
+        {
+            PerformDance(_chosenDance, ifRight);
+        }
+
+        else Debug.Log("Error no appropriate dance pose found!");
+    }
+
     public void PerformDance(DancePose dancePose, bool ifRight = false)
     {
-        spriteRenderer.flipY = ifRight;
+        spriteRenderer.flipX = ifRight;
 
         //Choose corresponding sprite based on dancePose received
-        //Todo: Need to be connected with Kato's script closer
         switch (dancePose)
         {
             case DancePose.scratchNote:
