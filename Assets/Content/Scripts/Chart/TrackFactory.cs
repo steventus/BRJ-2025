ab9736cd152a21ace2d11b44fe27400bb0d9ee9d
Steventus <56802489+steventus@users.noreply.github.com>
2025-01-18 07:06:33 +0800

    Chart and Chart Integration

--- Assets/Content/Scripts/Chart/TrackFactory.cs
+++ Assets/Content/Scripts/Chart/TrackFactory.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TrackFactory : MonoBehaviour
 {
     public static TrackFactory instance = null;
-    public List<RectTransform> notes;
+    public GameObject notePrefab;
+    public RectTransform track;
+    public GameObject currentChartPrefab;
+    private Chart currentChart => currentChartPrefab.GetComponent<Chart>();
+    public List<GameObject> notes;
 
+    [Header("Dynamic Track Generation Settings")]
     //Set Track Length
     public float MinLength; //Base length at minimum notes
     public float LengthPerBeat; //Additional length (gradient) per additional notes
@@ -28,12 +34,35 @@ public class TrackFactory : MonoBehaviour
     {
     }
 
-    public void CreateTrack(){
-        //Receive data from ChartMaker
+    void Update()
+    {
+        #region TODO: REMOVE WHEN FINISHED
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            CreateTrack();
+        }
+        #endregion
+    }
 
-        //Instantiate track
+    public void CreateTrack()
+    {
+        ClearTrack();
 
-        //Count Notes present in track and update notes
+        //Receive data from ChartMaker and instantiate new notes under track
+        for (int i = 0; i <= currentChart.notes.Count - 1; i++)
+        {
+            Note _currentSprite = currentChart.notes[i];
+
+            GameObject _obj = Instantiate(notePrefab);
+
+            Image _objImage = _obj.GetComponent<Image>();
+            _objImage.sprite = _currentSprite.GetComponentInChildren<SpriteRenderer>().sprite;
+            _objImage.color = _currentSprite.GetComponentInChildren<SpriteRenderer>().color;
+
+            _obj.transform.SetParent(track, false);
+
+            notes.Add(_obj);
+        }
 
         AdjustTrackLength();
     }
@@ -42,6 +71,16 @@ public class TrackFactory : MonoBehaviour
     {
         //Adjust distance
         GetComponent<RectTransform>().sizeDelta = new Vector2(MinLength + notes.Count * LengthPerBeat, trackHeight);
+    }
+
+    void ClearTrack()
+    {
+        for (int i = notes.Count - 1; i >= 0; i--)
+        {
+            Destroy(notes[i].gameObject);
+        }
+
+        notes.Clear();
     }
 
     void OnValidate()
